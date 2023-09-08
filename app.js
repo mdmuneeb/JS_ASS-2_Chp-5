121300f819ec7deb032d194efa1a4675404e7172
mdmuneeb <muneebzafar999@gmail.com>
2023-09-08 04:41:37 -0700

    Completed

---
+++ app.js
@@ -0,0 +1,177 @@
+// ? chapter 5
+
+// ! Question 1
+ 
+// var inp1 = +prompt ("Enter first numer: "),
+// inp2 = +prompt("Enter second number: ");
+
+// var result = inp1+inp2;
+
+// document.write ("Sum of "+inp1+" and"+inp2+" is "+result);
+
+
+// ! Question 2
+
+// * 1)
+
+// var inp1 = +prompt ("Enter first numer: "),
+// inp2 = +prompt("Enter second number: ");
+
+// var result = inp1-inp2;
+
+// document.write ("Sum of "+inp1+" and"+inp2+" is "+result);
+
+// * 2)
+
+// var inp1 = +prompt ("Enter first numer: "),
+// inp2 = +prompt("Enter second number: ");
+
+// var result = inp1-inp2;
+
+// document.write ("Sum of "+inp1+" and"+inp2+" is "+result);
+
+// * 3)
+
+// var inp1 = +prompt ("Enter first numer: "),
+// inp2 = +prompt("Enter second number: ");
+
+// var result = inp1*inp2;
+
+// document.write ("Sum of "+inp1+" and"+inp2+" is "+result);
+
+// * 4)
+
+// var inp1 = +prompt ("Enter first numer: "),
+// inp2 = +prompt("Enter second number: ");
+
+// var result = inp1/inp2;
+
+// document.write ("Sum of "+inp1+" and"+inp2+" is "+result);
+
+
+// ! Question 3
+
+// var variable;
+// document.write("Value after variable decleration is: "+variable); 
+// variable = 5
+// document.write(" <br> Initial Value: "+variable);
+// variable+=1;
+// document.write(" <br> Value after Incrementation: "+variable);
+// variable+=7;
+// document.write(" <br> Value after Addition: "+variable);
+// variable-=1;
+// document.write(" <br> Value after Subtration: "+variable);
+// if(variable%3===0){
+//     document.write(" <br> The remainder is: 0")
+// }
+
+// ! Question 4
+
+// var cost = 600;
+// var ticket = +prompt("Enter total amount of tickets you want: ");
+// document.write("Total cost to buy "+ticket+" tickets to a movie is "+cost*ticket+" PKR");
+
+
+// ! Question 5
+
+// var tableNUmber = +prompt("Enter the multiplier");
+
+// document.write("Table of "+tableNUmber,"<br>");
+// document.write(tableNUmber,"*","1","=",tableNUmber*1,"<br>");
+// document.write(tableNUmber,"*","2","=",tableNUmber*2,"<br>");
+// document.write(tableNUmber,"*","3","=",tableNUmber*3,"<br>");
+// document.write(tableNUmber,"*","4","=",tableNUmber*4,"<br>");
+// document.write(tableNUmber,"*","5","=",tableNUmber*5,"<br>");
+// document.write(tableNUmber,"*","6","=",tableNUmber*6,"<br>");
+// document.write(tableNUmber,"*","7","=",tableNUmber*7,"<br>");
+// document.write(tableNUmber,"*","8","=",tableNUmber*8,"<br>");
+// document.write(tableNUmber,"*","9","=",tableNUmber*9,"<br>");
+// document.write(tableNUmber,"*","10","=",tableNUmber*10,"<br>");
+
+// ! Question 6
+
+// var Celsius = +prompt("Enter Centigrade: ");
+// var Farhenhiet = +prompt("Enter Farhenheit: ");
+
+// document.write(Celsius,"C","is",Celsius*9/5+32,"F","<br>");
+// document.write(Farhenhiet,"F","is",(Farhenhiet-32)*5/9,"C");
+
+// ! Question 7
+
+// var priceItem1 = 650,
+// quantity1 = 3,
+// priceItem2 = 100,
+// quantity2 = 7,
+// shippingCharges = 100;
+
+// document.write("<h1>Shopping Cart</h1>")
+// document.write("Price of item 1 is ",priceItem1,"<br>");
+// document.write("Quantity of item 1 is ",quantity1,"<br>");
+// document.write("Price of item 2 is ",priceItem2,"<br>");
+// document.write("Quantity of item 2 is ",quantity2,"<br>");
+// document.write("Shipping Charges ",shippingCharges,"<br>");
+
+// document.write("Total cost of your order is ",(priceItem1*quantity1)+(priceItem2*quantity2)+shippingCharges)
+
+
+// ! Question 8
+
+// var marks = 804,
+// totalMarks = 980;
+
+// document.write("Total Marks: ",totalMarks,"<br>");
+// document.write("Marks Obtained: ",marks);
+
+// document.write("Percentage: ",(marks/totalMarks)*100);
+
+// ! Question 9
+
+// var US = 306.33,
+// suadiRiyals = 81.67,
+// quantityUS = 10,
+// quantityRiyal = 25
+
+// document.write("<h1>Currency in PKR</h1>")
+// document.write("Total Currency in PKR: ",(US*quantityUS)+(suadiRiyals*quantityRiyal));
+
+// ! Question 10
+
+// var number = 2;
+
+// document.write("The final calculation is ",((number+5)*10)/2);
+
+// ! Question 11
+
+// var currentYear = 2023,
+// birthYear = 2004;
+
+// document.write("Current Year: ",currentYear,"<br>");
+// document.write("Birth Year: ",birthYear,"<br>");
+// document.write("Year: ",2023-2004);
+
+// ! Question 12
+
+// var radius = 20
+// document.write("<h1>Geometrizer</h1>")
+
+// document.write("Radius: ",radius,"<br>")
+// document.write("The circumference is: ",2*3.142*radius,"<br>");
+// document.write("The area is: ",3.142*radius**2);
+
+
+// ! Question 13
+
+// var favouriteSnack = "Lays",
+// currentAge = 15,
+// maximumAge = 65,
+// snackPerDay = 3;
+
+// document.write("<h1>The Life Time Supply Calculator</h1>")
+// document.write("Favourite Snack: "+favouriteSnack+"<br>");
+// document.write("Current age: "+currentAge+"<br>");
+// document.write("Estimated Maximum Age: "+maximumAge+"<br>");
+// document.write("Amount of snacks per day: "+snackPerDay+"<br>");
+
+// document.write("You will need "+ (maximumAge-currentAge)*snackPerDay+" "+favouriteSnack+" to last you untill the recipe old age of "+maximumAge)
+
+
